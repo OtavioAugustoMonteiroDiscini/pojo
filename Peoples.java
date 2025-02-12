0e2bd42b2f3108cd9e3ae2a046e06f2f00eeb60d
Otavio Augusto Monteiro Discini <otavio2005discini@gmail.com>
2025-02-12 17:03:10 +0000

    Salvando o primeiro arquivo da Aula

---
+++ Peoples.java
@@ -0,0 +1,21 @@
+public class Peoples {
+    private String nome;
+    private String idade;
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
